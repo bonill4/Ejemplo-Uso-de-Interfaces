8709269057124b389a410799e5c987d6c29ce3d7
bonill4 <williamsbonill11@gmail.com>
2024-01-25 19:58:41 -0600

    Commit #1 Pequeño ejercicio sobre el uso de interfaces

---
+++ AvisosTrafico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    internal class AvisosTrafico : IAvisos
+    {
+
+        public AvisosTrafico() 
+        {
+
+            remitente = "DGT";
+
+            mensaje = "Sancion cometida, Pague antes de 3 dias y se beneficiara de una reduccion en la sancion del 50%";
+
+            fecha = "";
+        
+        }
+
+        public AvisosTrafico(String remitente, String mensaje, String fecha)
+        {
+
+            this.remitente = remitente;
+
+            this.mensaje = mensaje;
+
+            this.fecha = fecha;
+
+        }
+
+        public string getFecha()
+        {
+            return fecha;
+        }
+
+        public void MostrarAviso()
+        {
+            Console.WriteLine($"{mensaje} ha sido enviado por {remitente} el dia {fecha} ");
+        }
+
+        private String remitente;
+
+        private String mensaje;
+
+        private String fecha;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             AvisosTrafico av1 = new AvisosTrafico();
+
+            av1.MostrarAviso();
+
+            AvisosTrafico av2 = new AvisosTrafico("Jefatura Provincial Madrid", "Sancion de velocidad $300.00", "20-05-24");
+
+            Console.WriteLine(av2.getFecha()); 
+
+            av2.MostrarAviso();
+        }
+    }
+}
